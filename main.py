03f6ad7e1365ad1f1d8093beb5837f0ed497c0fb
seml404 <4040591@gmail.com>
2022-08-28 21:41:08 +0300

    created func

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+
+
+
+ts_folder = "/Code/type-script-course"
+ts_content = os.scandir(ts_folder)
+
+
+def check_and_delete(file, target_folder):
+    if os.path.isfile(file) and str(file).find('.js')>0:
+        print('file', file, os.path.join( target_folder, file))
+        os.remove(os.path.join(target_folder, file))
+
+def remove_js(folder_name):
+    subfolders = []
+    for item in folder_name:
+        if os.path.isdir(item):
+            subfolders.append(item);
+            print(subfolders)
+
+    for subfolder in subfolders:
+        files = os.scandir(subfolder)
+        for file in files:
+            check_and_delete(file, subfolder);
+
+
+
+
+
+remove_js(ts_content);
