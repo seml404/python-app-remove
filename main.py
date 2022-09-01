5fc8c656bf96f2b4af24785d9fc8822183d41e86
seml404 <4040591@gmail.com>
2022-09-01 22:36:20 +0300

    small amendment

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import os
 
 
 
-ts_folder = "/Code/type-script-course"
+ts_folder = "/Code/type-script-course/src"
 ts_content = os.scandir(ts_folder)
 
 
 def check_and_delete(file, target_folder):
-    if os.path.isfile(file) and str(file).find('.js')>0:
+    if os.path.isfile(file) and (str(file).find('.js')>0 or str(file).find('.js.map')>0 or str(file).find('.d.ts')>0 ):
         print('file', file, os.path.join( target_folder, file))
         os.remove(os.path.join(target_folder, file))
 
